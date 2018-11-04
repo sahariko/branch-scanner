f8f81c2d2a078dc45457622054de1d1709c66ca3
Sahar Brodbeker <saharonb@gmail.com>
2018-11-04 20:54:28 +0200

    Fix execution order bug

--- branch-scanner.js
+++ branch-scanner.js
@@ -10,13 +10,13 @@ const DIRECTORY_ARG_DEFAULT_MESSAGE = 'the current directory';
 program
     .usage('[opts]')
     .description(description)
-    .version(version, '-v, --version', 'Print the program\'s version number')
+    .version(version, '-v, --version', 'print the program\'s version number')
     .option('-d, --directory [path]', 'specify a directory to scan', DIRECTORY_ARG_DEFAULT_MESSAGE);
+
+program.parse(process.argv);
 
 const { directory } = program;
 const directorySpecified = directory && directory !== DIRECTORY_ARG_DEFAULT_MESSAGE;
 const workingDirectory = directorySpecified ? path.resolve(__dirname, directory) : process.cwd();
-
-program.parse(process.argv);
 
 main(workingDirectory);
