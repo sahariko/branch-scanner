777e407e566bb0b9259cee90cb83e7f07a90ed66
Sahar Brodbeker <saharonb@gmail.com>
2018-11-04 12:18:21 +0200

    Add a more extensive build process and split to modules

--- index.js
+++ index.js
@@ -1,49 +1,18 @@
-const fs = require('fs');
-const { join } = require('path');
-const { exec } = require('child_process');
-
-const GIT_FOLDER_NAME = '.git';
-const IGNORED_BRANCHES = ['master'];
+import path from 'path';
+import colorizeFactory from './lib/colorizeFactory';
+import gelAllSubdirectories from './lib/gelAllSubdirectories';
+import listDirectoryBranches from './lib/listDirectoryBranches';
 
 const currentDirectory = process.cwd();
 
-const colorizeFactory = (color) => (text) => `${color}${text}\x1b[0m`;
-
-const colorizeCyan = colorizeFactory('\x1b[36m');
-
 const colorizeBold = colorizeFactory('\x1b[1m');
-
-const listDirectoryBranches = (directory) => {
-    const directoryPath = join(currentDirectory, directory);
-    const directoryFiles = fs.readdirSync(directoryPath);
-
-    if (!directoryFiles.includes(GIT_FOLDER_NAME)) return;
-
-    exec('git for-each-ref --format=\'%(refname:short)\' refs/heads', {
-        cwd: directoryPath
-    }, (error, stdout, stderr) => {
-        if (error) throw new Error(error);
-        if (stderr) throw new Error(stderr);
-
-        const branches = stdout.split('\n').filter((branch) => (
-            branch && !IGNORED_BRANCHES.includes(branch)
-        ));
-
-        if (branches.length) {
-            console.log(colorizeCyan(`${directory} (${branches.length})`));
-            console.log(branches.join('\n'), '\n');
-        }
-
-    });
-};
-
-const directories = fs.readdirSync(currentDirectory).filter((file) => {
-    const isHiddenFile = file.charAt(0) === '.';
-    const isDirectory = fs.statSync(file).isDirectory();
-
-    return !isHiddenFile && isDirectory;
-});
 
 console.log(colorizeBold(`🤖  Scanning all directories under ${currentDirectory}\n`));
 
-directories.forEach(listDirectoryBranches);
+const directories = gelAllSubdirectories(currentDirectory);
+
+directories.forEach((directory) => {
+    const directoryPath = path.join(currentDirectory, directory);
+
+    listDirectoryBranches(directoryPath);
+});
